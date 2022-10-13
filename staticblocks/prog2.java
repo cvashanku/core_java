82ce2d5f3a00a34fc3fac102668380c448b1cb97
cvashanku <cvashankar18@gmail.com>
2022-10-13 10:44:29 +0530

    static blocks

---
+++ staticblocks/prog2.java
@@ -0,0 +1,30 @@
+class demo
+{
+ int x=10;//instance variable
+ static int y=20;//static variable
+ void show()//instance method
+{
+
+}
+static void display()//class method
+{
+
+}
+{}//block
+static//static block
+{
+
+}
+demo()//default constructor
+{
+
+}
+demo(int x)//parameterized constructor
+{
+
+}
+public static void main(String[] args)//main method
+{
+
+}
+}
---
+++ staticblocks/prog3.java
@@ -0,0 +1,12 @@
+class demo
+{
+ static int x=10;
+ static void show()
+ {
+ System.out.println(x);
+ }
+ public static void main(String[] args)
+ { 
+  demo.show();
+ }
+}
