eee1c811ad33b9fa293acf7e27d71f9f407c9f51
cvashanku <cvashankar18@gmail.com>
2022-10-15 10:21:28 +0530

    blocks

---
+++ blocks/prog1.java
@@ -0,0 +1,17 @@
+class demo
+{
+ {
+  System.out.println("block1");
+ }
+ demo()
+ {
+  System.out.println("constructor");
+ }
+public static void main(String[] args)
+{
+ new demo();
+}
+{
+ System.out.println("block2");
+}
+}
---
+++ blocks/prog2.java
@@ -0,0 +1,19 @@
+
+class demo
+{
+ {
+  System.out.println("block1");
+ }
+ demo()
+{
+ System.out.println("constructor");
+}
+public static void main(String[] args)
+{
+ new demo();
+ System.out.println("main method");
+}
+ {
+  System.out.println("block2");
+ }
+}
