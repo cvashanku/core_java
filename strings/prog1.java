707681dca007d36bcdc0ae40d8d4d8ce3fb34812
cvashanku <cvashankar18@gmail.com>
2022-11-23 11:30:06 +0530

    strings

---
+++ strings/prog1.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class demo 
+{
+ public static void main(String args[])
+ {
+   String s=new String();
+   System.out.println(s);
+   demo d=new demo();
+   System.out.println(d);
+ }
+}
---
+++ strings/prog2.java
@@ -0,0 +1,14 @@
+class demo
+{
+ public String toString()
+ {
+   return" ";
+ }
+  public static void main(String args[])
+ { 
+  String s=new String();
+  System.out.println(s);
+  demo d=new demo();
+  System.out.println(d);
+ }
+}
