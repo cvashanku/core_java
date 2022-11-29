858ad6d4300a46275e8982bfe54bcab4b37c8009
cvashanku <cvashankar18@gmail.com>
2022-11-29 17:57:34 +0530

    java streams

--- java-streams/prog4.java
+++ java-streams/prog4.java
@@ -1,7 +1,6 @@
 //Implement a program to count number of digits present in the given number?
 
 import java.util.*;
-
 class demo
 {
   static int c=0;
@@ -23,6 +22,7 @@ class test
      Scanner obj=new Scanner(System.in);
      System.out.println("enter any number:");
      int n=obj.nextInt();
+
      System.out.println(demo.count(n));
    }
 }
