39076212f995bdc47fbed538d3b7c5a205f65263
cvashanku <cvashankar18@gmail.com>
2022-11-29 17:51:12 +0530

    recursion

--- java-streams/prog3.java
+++ java-streams/prog3.java
@@ -6,12 +6,17 @@ class demo
 {
   public static void main(String args[])
    {
+    try{
      FileInputStream fis=new FileInputStream(args[0]);
      int n=fis.available();
      byte[] b=new byte[n];
      fis.read(b);
      String s=new String(b);
      System.out.println(s);
-     fis close();
+     File close();
+     }catch(Exception e)
+      {
+        System.err.println(e);
+      }
    }
 }
